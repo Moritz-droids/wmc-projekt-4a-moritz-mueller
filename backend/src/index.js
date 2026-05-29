d5c87391a63b9646a7ef262eae3f1ab8ee9088dc
Moritz-droids <muellerm22083@sus.htl-grieskirchen.at>
2026-05-29 11:57:34 +0200

    added movieRoutes to index.js and made useful request examples

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 
 import authRoutes from "./routes/auth.routes.js";
 import roomRoutes from "./routes/room.routes.js";
+import movieRoutes from "./routes/movie.routes.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/rooms", roomRoutes);
+app.use("/api", movieRoutes);
 
 app.get("/", (req, res) => {
   res.send("Movie Night Planner Backend läuft");
